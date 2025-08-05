aed883b6be1b23f8162bf0e501ff3f61b05cfe22
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-08-05 15:30:39 +0300

    fix deployment bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable no-constant-condition */
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Layout from "./pages/admin/Layout";
 import Dashboard from "./pages/admin/Dashboard";
 import ListBlog from "./pages/admin/ListBlog";
-import AddBlog from "./pages/admin/Addblog";
+import AddBlog from "./pages/admin/AddBlog";
 import Comments from "./pages/admin/Comments";
 import Login from "./components/admin/Login";
 import "quill/dist/quill.snow.css";
